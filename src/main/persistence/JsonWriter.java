19d657bebd2fe1e314c808e9481ee0db82592b06
Melissa <medlin@students.cs.ubc.ca>
2021-08-02 12:42:12 -0700

    final data persistence version

--- src/main/persistence/JsonWriter.java
+++ src/main/persistence/JsonWriter.java
@@ -22,12 +22,6 @@ public class JsonWriter {
         this.destination = destination;
     }
 
-    //MODIFIES: this
-    //EFFECTS: sets destination of writer
-    public void setDestination(String destination) {
-        this.destination = destination;
-    }
-
     // MODIFIES: this
     // EFFECTS: opens writer; throws FileNotFoundException if destination file cannot
     // be opened for writing
