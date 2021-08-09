4a68a402a7e0ec52539f85c2dd7b02781741fb73
Melissa <medlin@students.cs.ubc.ca>
2021-08-09 13:30:56 -0700

    added saving/loading functionality

---
+++ src/main/ui/MenuGUI.java
@@ -0,0 +1,15 @@
+package ui;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MenuGUI extends JPanel {
+
+    public MenuGUI() {
+        super();
+        setBackground(Color.pink);
+        setBounds(0,900,1000,100);
+        setVisible(true);
+    }
+
+}
--- src/main/ui/WorkoutAppGUI.java
+++ src/main/ui/WorkoutAppGUI.java
@@ -6,16 +6,36 @@ package ui;
 // https://www.youtube.com/watch?v=Kmgo00avvEw
 
 import model.Workout;
+import persistence.JsonReader;
+import persistence.JsonWriter;
 
 import javax.swing.*;
 import javax.swing.border.Border;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
-public class WorkoutAppGUI extends JFrame {
+public class WorkoutAppGUI extends JFrame implements ActionListener {
     private static final int FRAME_WIDTH = 1000;
     private static final int FRAME_HEIGHT = 1000;
+    private static final String SLOT_ONE = "./data/slotOne.json";
+    private static final String SLOT_TWO = "./data/slotTwo.json";
     private Workout workout1;
     private Workout workout2;
+    private JPanel panel1;
+    private JPanel panel2;
+    private MenuGUI menu;
+    private JMenuBar menuBar;
+    private JMenu fileMenu;
+    private JMenu editMenu;
+    private JMenuItem loadItemOne;
+    private JMenuItem loadItemTwo;
+    private JMenuItem saveItemOne;
+    private JMenuItem saveItemTwo;
+    private JsonWriter jsonWriter;
+    private JsonReader jsonReader;
 
     //EFFECTS: constructs appgui
     public WorkoutAppGUI() {
@@ -27,45 +47,121 @@ public class WorkoutAppGUI extends JFrame {
 
         init();
 
-        //workout panel 1
-        JPanel panel1 = new WorkoutGUI(workout1, Color.red, 0, 0);
-        add(panel1);
-        panel1.setVisible(true);
+        refreshDisplay();
 
-        //workout panel 2
-        JPanel panel2 = new WorkoutGUI(workout2, Color.yellow, 500,0);
-        add(panel2);
-        panel2.setVisible(true);
-
-
-
-        JPanel menu = new JPanel();
-        menu.setBackground(Color.pink);
-        menu.setBounds(0,900,1000,100);
-        add(menu);
-        menu.setVisible(true);
-
+        //initialize menu bar
+        menuBar = new JMenuBar();
+        menuBarSetup();
+        setJMenuBar(menuBar);
 
         setVisible(true);
 //        pack();
 
+    }
 
+    private void refreshDisplay() {
+        //workout panel 1
+        panel1 = new WorkoutGUI(workout1, Color.red, 0, 0);
+        add(panel1);
+        panel1.setVisible(true);
 
+        //workout panel 2
+        panel2 = new WorkoutGUI(workout2, Color.yellow, 500,0);
+        add(panel2);
+        panel2.setVisible(true);
+
+        menu = new MenuGUI();
+        add(menu);
+    }
+
+    //MODIFIES: this
+    //EFFECTS: initializes menu bar
+    private void menuBarSetup() {
+        fileMenu = new JMenu("File");
+        editMenu = new JMenu("Edit");
+
+        menuBar.add(fileMenu);
+        menuBar.add(editMenu);
+
+        menuItemSetup();
+    }
+
+    private void menuItemSetup() {
+        loadItemOne = new JMenuItem("Load " + workout1.getName());
+        loadItemTwo = new JMenuItem("Load " + workout2.getName());
+        saveItemOne = new JMenuItem("Save " + workout1.getName());
+        saveItemTwo = new JMenuItem("Save " + workout2.getName());
+
+        fileMenu.add(loadItemOne);
+        fileMenu.add(loadItemTwo);
+        fileMenu.add(saveItemOne);
+        fileMenu.add(saveItemTwo);
+
+        loadItemOne.addActionListener(this);
+        loadItemTwo.addActionListener(this);
+        saveItemOne.addActionListener(this);
+        saveItemTwo.addActionListener(this);
     }
 
     //EFFECTS: initializes workouts
     private void init() {
-        workout1 = new Workout("day1", "goodluck!");
-        workout1.addExercise("backsquat", 3,5,130);
-        workout1.addExercise("snatch", 3,2,65);
-        workout1.addExercise("clean",3,100,20000);
-        workout1.addExercise("sndeadlift", 3,3,91);
-        workout1.setCoachComment("yo");
-
-        workout2 = new Workout("day2");
-        workout2.addExercise("bench", 1, 8, 130);
-        workout2.addExercise("chin-up",3,5,0);
-        workout2.addExercise("bicep curl",3,12,25);
-        workout2.setCoachComment("YO");
+        jsonWriter = new JsonWriter(SLOT_ONE);
+        jsonReader = new JsonReader(SLOT_ONE);
     }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == loadItemOne) {
+            loadWorkoutOne();
+            refreshDisplay();
+        } else if (e.getSource() == loadItemTwo) {
+            loadWorkoutTwo();
+            refreshDisplay();
+        } else if (e.getSource() == saveItemOne) {
+            saveWorkout(SLOT_ONE, workout1);
+            refreshDisplay();
+        } else if (e.getSource() == saveItemTwo) {
+            saveWorkout(SLOT_TWO, workout2);
+            refreshDisplay();
+        }
+    }
+
+    // EFFECTS: saves workout to file
+    private void saveWorkout(String slot, Workout workout) {
+        jsonWriter.setDestination(slot);
+
+        try {
+            jsonWriter.open();
+            jsonWriter.write(workout);
+            jsonWriter.close();
+            System.out.println("Saved " + workout.getName() + " to " + slot);
+        } catch (FileNotFoundException e) {
+            System.out.println("Unable to write to file: " + slot);
+        }
+    }
+
+    // MODIFIES: this
+    // EFFECTS: loads workout from file one
+    private void loadWorkoutOne() {
+        try {
+            jsonReader.setSource(SLOT_ONE);
+            workout1 = jsonReader.read();
+            System.out.println("Loaded " + workout1.getName() + " from " + SLOT_ONE);
+        } catch (IOException e) {
+            System.out.println("Unable to read from file: " + SLOT_ONE);
+        }
+    }
+
+    // MODIFIES: this
+    // EFFECTS: loads workout from file two
+    private void loadWorkoutTwo() {
+        try {
+            jsonReader.setSource(SLOT_TWO);
+            workout2 = jsonReader.read();
+            System.out.println("Loaded " + workout2.getName() + " from " + SLOT_TWO);
+        } catch (IOException e) {
+            System.out.println("Unable to read from file: " + SLOT_TWO);
+        }
+    }
+
 }
